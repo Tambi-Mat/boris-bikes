2b0ccf40292f161498f19e654b9593c44106ef7b
Tambi-Mat <caicaratambi@gmail.com>
2017-07-04 16:58:40 +0100

    edit so that docking_station does not create infinite bikes

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,10 +1,25 @@
 require "docking_station"
 
 describe DockingStation do
+
+  describe "#release_bike"do
+    it "releases a bike" do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike
+    end
+
+    it "raises an error when there are no bikes available" do
+      expect{ subject.release_bike }.to raise_error "No bikes available"
+    end
+  end
+
   it { is_expected.to respond_to :release_bike}
 
   it "releasing working bikes" do
-    bike = subject.release_bike
+    bike = Bike.new
+    subject.dock(bike)
+    subject.release_bike
     expect(bike).to be_working
   end
 
@@ -18,4 +33,6 @@ describe DockingStation do
     subject.dock(bike)
     expect(subject.bike).to eq bike
   end
+
+
 end
